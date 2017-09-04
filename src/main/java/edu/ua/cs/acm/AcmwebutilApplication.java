21272f1d7b5b4a29b85276474f90eacaf1adac26
Jake Zarobsky <jzarob@gmail.com>
2017-09-04 14:09:30 -0500

    Removed the bean config file

--- src/main/java/edu/ua/cs/acm/AcmwebutilApplication.java
+++ src/main/java/edu/ua/cs/acm/AcmwebutilApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AcmwebutilApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AcmwebutilApplication.class, args);
 	}
